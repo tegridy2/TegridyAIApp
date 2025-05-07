f6c2d54d3d0705bb9169d061fbb760bf9353c901
Tegridy <dannelleycody87@gmail.com>
2025-05-07 12:39:47 -0400

    Initial commit

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,11 @@
+package com.tegridyai.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
